2855debf83f90040d1607be5cda4e83bf6703da8
Kristiano <kristianokasper@gmail.com>
2022-04-12 16:10:38 -0300

    feat: cria interface para Motorcycle (Req 18 OK)

---
+++ src/interfaces/MotorcycleInterface.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+import { VehicleSchema } from './VehicleInterface';
+
+const MotorcycleSchema = VehicleSchema.extend({
+  category: z
+    .enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z
+    .number({
+      required_error: 'Engine capacity is required',
+      invalid_type_error: 'Engine capacity must be a number',
+    })
+    .min(0, 'Engine capacity must be 0 or higher')
+    .max(2500, 'Engine capacity must be 2500 or lower'),
+});
+
+type Motorcycle = z.infer<typeof MotorcycleSchema>;
+
+export { Motorcycle, MotorcycleSchema };
