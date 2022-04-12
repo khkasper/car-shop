64f05ea0040d7d846d1dd1f674c0eea25efa8f1c
Kristiano <kristianokasper@gmail.com>
2022-04-11 22:59:21 -0300

    feat: cria controller para Car (Req 04)

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -1,19 +1,98 @@
-// import Controller from '.';
-// import ExampleService from '../services/ExampleService';
-// import { Example } from '../interfaces/ExampleInterface';
+import { Request, Response } from 'express';
+import Controller, { RequestWithBody, ResponseError } from '.';
+import CarService from '../services/Car';
+import { Car } from '../interfaces/CarInterface';
 
-// export default class ExampleController extends Controller<Example> {
-//   private $route: string;
+class CarController extends Controller<Car> {
+  private _route: string;
 
-//   constructor(
-//     service = new ExampleService(),
-//     route = '/Examples',
-//   ) {
-//     super(service);
-//     this.$route = route;
-//   }
+  constructor(
+    service = new CarService(),
+    route = '/cars',
+  ) {
+    super(service);
+    this._route = route;
+  }
 
-//   get route() { return this.$route; }
+  get route() { return this._route; }
 
-//  > Seus métodos vão aqui <
-// }
+  create = async (
+    req: RequestWithBody<Car>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { body } = req;
+    try {
+      const car = await this.service.create(body);
+      if (!car) {
+        return res.status(500).json({ error: this.errors.internal });
+      }
+      if ('error' in car) {
+        return res.status(400).json(car);
+      }
+      return res.status(201).json(car);
+    } catch (err) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  read = async (
+    req: Request<{ id: string }>,
+    res: Response<Car[] | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const cars = await this.service.read();
+      return cars
+        ? res.json(cars)
+        : res.status(404).json({ error: this.errors.notFound });
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  readOne = async (
+    req: Request<{ id: string }>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    try {
+      const car = await this.service.readOne(id);
+      return car
+        ? res.json(car)
+        : res.status(404).json({ error: this.errors.notFound });
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  update = async (
+    req: Request<{ id: string, obj: Car }>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    try {
+      const car = await this.service.update(id, req.body);
+      return car
+        ? res.json(car)
+        : res.status(404).json({ error: this.errors.notFound });
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  delete = async (
+    req: Request<{ id: string }>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    try {
+      const car = await this.service.delete(id);
+      return car
+        ? res.json(car)
+        : res.status(404).json({ error: this.errors.notFound });
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+}
+
+export default CarController;
