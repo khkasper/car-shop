880e982938033679d8fd2e84af0b1e968e1b3d0e
Kristiano <kristianokasper@gmail.com>
2022-04-11 21:56:17 -0300

    feat: cria Model para Car (Req 04)

---
+++ src/models/Car.ts
@@ -0,0 +1,23 @@
+import { Schema, model as createModel, Document } from 'mongoose';
+import { Car } from '../interfaces/CarInterface';
+import MongoModel from './MongoModel';
+
+interface CarDocument extends Car, Document { }
+
+const carSchema = new Schema<CarDocument>({
+  model: String,
+  year: String,
+  color: String,
+  status: Boolean || undefined,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class CarModel extends MongoModel<Car> {
+  constructor(model = createModel('Cars', carSchema)) {
+    super(model);
+  }
+}
+
+export default CarModel;
